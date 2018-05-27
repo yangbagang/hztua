6db020cbf90fb2ced221b4d0f6386057d39ae30d
yangbagang <81667842@qq.com>
2018-05-27 17:48:18 +0800

    fix bugs

--- grails-app/services/com/ybg/hzt/device/DeviceInfoService.groovy
+++ grails-app/services/com/ybg/hzt/device/DeviceInfoService.groovy
@@ -10,8 +10,8 @@ class DeviceInfoService {
 
     def list(Long userId, String name) {
         def sql = new Sql(dataSource)
-        def query = "select a.name, b.*, g.name as catalog from user_battery u left join device_info a on u.uid=a.uid " +
-                "left join device_value b on u.uid=b.uid left join device_catalog g on a.device_info_id = g.id" +
+        def query = "select a.name, b.*, g.name as catalogName,g.id as catalogId from user_battery u left join device_info a " +
+                "on u.uid=a.uid left join device_value b on u.uid=b.uid left join device_catalog g on a.device_info_id = g.id" +
                 "where a.user_info_id=? and a.name like ?"
         sql.rows(query, userId, "%" + name + "%")
     }
